2d56e7a879fdeded07ce0cd6696e6978d3512b8c
sorlros <sorlros2047@gmail.com>
2023-09-01 15:10:57 +0900

    github callback Url fixed

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -92,12 +92,7 @@ const Auth = () => {
 						</button>
 						<div className="flex flex-row items-center gap-4 mt-8 justify-center">
 							<div
-								onClick={() =>
-									signIn("google", {
-										callbackUrl:
-											"https://netflix-clone-bice-pi.vercel.app/api/auth/callback/github",
-									})
-								}
+								onClick={() => signIn("google", { callbackUrl: "/profiles" })}
 								className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
 							>
 								<FcGoogle size={30} />
@@ -105,7 +100,7 @@ const Auth = () => {
 							<div
 								onClick={() =>
 									signIn("github", {
-										callbackUrl: "/profiles",
+										callbackUrl: "https://netflix-clone-bice-pi.vercel.app/api/auth/callback/github",
 									})
 								}
 								className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
