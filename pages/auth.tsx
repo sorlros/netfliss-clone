b7626d6c69630273e401e0fa4a068cec057a37ca
sorlros <sorlros2047@gmail.com>
2023-09-01 16:17:15 +0900

    fixed

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -118,10 +118,7 @@ const Auth = () => {
 							</div>
 							<div
 								onClick={() =>
-									signIn("github", {
-										callbackUrl:
-											"/profiles",
-									})
+									signIn("github")
 								}
 								className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
 							>
