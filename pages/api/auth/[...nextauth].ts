71c9ef5b281c15d4bef3e8aac9abbcb7079d8007
sorlros <sorlros2047@gmail.com>
2023-09-04 14:30:16 +0900

    fixed

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,9 @@
 import NextAuth, { AuthOptions } from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import { compare } from "bcrypt";
-
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-
+import Credentials from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
+import { compare } from "bcrypt";
 import prismadb from "@/lib/prismadb";
 
 export const authOptions: AuthOptions = {
@@ -20,7 +17,7 @@ export const authOptions: AuthOptions = {
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
 		}),
 		Credentials({
-			id: "Credentials",
+			id: "credentials",
 			name: "Credentials",
 			credentials: {
 				email: {
@@ -29,12 +26,12 @@ export const authOptions: AuthOptions = {
 				},
 				password: {
 					label: "Password",
-					type: "password",
+					type: "passord",
 				},
 			},
 			async authorize(credentials) {
 				if (!credentials?.email || !credentials?.password) {
-					throw new Error("이메일 혹은 비밀번호가 틀렸습니다.");
+					throw new Error("Email and password required");
 				}
 
 				const user = await prismadb.user.findUnique({
@@ -44,13 +41,13 @@ export const authOptions: AuthOptions = {
 				});
 
 				if (!user || !user.hashedPassword) {
-					throw new Error("이메일이 존재하지 않습니다.");
+					throw new Error("Email does not exist");
 				}
 
 				const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
 
 				if (!isCorrectPassword) {
-					throw new Error("비밀번호가 틀렸습니다.");
+					throw new Error("Incorrect password");
 				}
 
 				return user;
@@ -62,9 +59,7 @@ export const authOptions: AuthOptions = {
 	},
 	debug: process.env.NODE_ENV === "development",
 	adapter: PrismaAdapter(prismadb),
-	session: {
-		strategy: "jwt",
-	},
+	session: { strategy: "jwt" },
 	jwt: {
 		secret: process.env.NEXTAUTH_JWT_SECRET,
 	},
